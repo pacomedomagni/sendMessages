369af66535491508d26872e9ca8fa51b22064efe
Pacome Domagni <pacomedomagni@gmail.com>
2020-09-02 23:47:17 -0500

    Pacome version of the Solution

--- src/SendTags.js
+++ src/SendTags.js
@@ -1,14 +1,16 @@
 import React, {useState} from 'react'
+/** assesment done by pacome domagni email pacomedomagni@gmail.com */
 
 export default function SendTags () {
     const [recipients, updateRecipients] = useState("")
     const [qualifier, updateQualifier] = useState("")
     const [sendTo, updateSendTo] = useState("")
     const [sendType, updateSendType] = useState("")
+    const [expression, updateExpression] = useState("")
     const [sent, updateSent] = useState(false)
 
     const handleChange = (event) => {
-        const value = event.target.value
+        const value = (event.target.value).toLowerCase()
         switch(event.target.name) {
             case "sendType":
                 updateSendType(value)
@@ -23,12 +25,63 @@ export default function SendTags () {
                 return;
         }
     }
+/** checking the validity of sendtype */
+    const validateSendType = (rawSendType)=>{
+
+        if(rawSendType ==='first name' || rawSendType === 'firstname') return 'firstname'
+        if(rawSendType ==='last name' || rawSendType === 'lastname') return 'lastname'          
+        if(rawSendType === 'organization' || rawSendType === 'organizationId') return 'organiationId'    
+        if(rawSendType === 'tag' || rawSendType === 'tags')  return 'tags'
+        
+        return
+    }
+    /* * formating Qualifier to adapt with url*/
+    const turnQualifier = (rawQualifier) => {
+        switch(rawQualifier){
+            case 'or':
+                return '_or'
+            case 'and':
+                return ''
+            default:
+                return -1
+        }
+
+    }
+    /** build the url format with our three parameters */
+    const buildUrl = (rQualifier, rSendType, rSendTo) =>{
+        let splitSendTo = rSendTo.split(',')
+        if(splitSendTo.length === 1){
+            return `search?${rSendType}=*${splitSendTo.join('')}*`
+        }else{
+            return `search${rQualifier}?${splitSendTo.map((result) => `${rSendType}[]=*${result.trim()}*&`).join('')}`
+        }
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        /*  implement me
-            hint: we will probably need to update state here to render the right parts
-        */
+        /** making sure the right inputs are enter otherwise throw an error message and return */
+        if(!validateSendType(sendType)||turnQualifier(qualifier) === -1){
+            updateExpression('')
+            updateSent(true)
+            updateRecipients('Wrong input please check and try again')
+            return
+        }
+        let params = buildUrl(turnQualifier(qualifier), validateSendType(sendType) , sendTo )
+
+        fetch(`https://sheetdb.io/api/v1/aka2sv6jd00dh/${params}`).then(response => response.json())
+        .then(data => {
+            /** convert raw data into String and format properly  */
+            let StringValue = ''
+            data.forEach(({firstName, lastName}) => StringValue+= firstName+' '+lastName+', ')
+            if(data.length < 1){
+                updateExpression('')
+                updateRecipients('No user was found!')
+            }else{              
+                updateExpression('Sent To: ')
+                updateRecipients(StringValue.slice(0 ,-2))
+            }
+            updateSent(true)
+        }).catch(err => console.log(err))
     }
 
     return (
@@ -50,7 +103,7 @@ export default function SendTags () {
                 </label>
                 <input type="submit" value="Send Messages" />
             </form>
-            { sent && <div>Sent to: {recipients}</div> }
+            { sent && <div>{expression} {recipients}</div> }
         </div>
     )
 }
